d39c008597f2c250f0afdac92bcc54b2cf822b71
roczekmichal <mich.rocz@gmail.com>
2020-10-10 11:20:20 +0200

    Print hello world, simply function for adding two numbers

--- cw1.cpp
+++ cw1.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
-using namespace std;
+
+int dodawanie(int a, int b){
+	return a+b;
+}
+
+
+int x;
+int z;
+
 
 int main(){
 
 
 
-    cout<<"Hello world"<<endl;
+
+//    std::cout<<"Hello world"<<std::endl;
+std::cout<<"First number: "<<std::endl;
+std::cin>>x;
+std::cout<<"Second number: "<<std::endl;
+std::cin>>z;
+std::cout<<"Adding two numbers: "<<dodawanie(x,z)<<std::endl;
+
+
 
     return 0;
 }
