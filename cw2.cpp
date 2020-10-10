2a3a5b0d8756e0f387eec98e1b459e54857e3169
roczekmichal <mich.rocz@gmail.com>
2020-10-10 12:31:24 +0200

    Loops and arrays

---
+++ cw2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+	
+	//FOR LOOP
+	/*
+	for(int i=0;i<10;i++){
+		std::cout<<"Hello"<<std::endl;
+	}
+	*/
+	
+	//WHILE LOOP
+	/*
+	int times,i;
+	std::cin>>times;
+	
+	while(i<times)
+	{
+		std::cout<<"text here"<<std::endl;
+		i++;
+	}
+	*/
+
+
+return 0;
+}
